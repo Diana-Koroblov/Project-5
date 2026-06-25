f700735b3bdd9af145cb04b64433de41e4eefd6f
Itay <itay.malich2@gmail.com>
2026-06-25 19:05:46 +0300

    Refactor __init__.py to include all necessary imports and define module exports

--- src/ex05/__init__.py
+++ src/ex05/__init__.py
@@ -1,3 +1,28 @@
 """EX05: Running a Massive LLM Locally with AirLLM and Quantization."""
 
+from ex05.config import EconomicsConfig, ExperimentConfig, OllamaConfig, get_hf_token
+from ex05.economics import (
+    compute_api_cost,
+    compute_cloud_gpu_cost,
+    compute_onprem_cost,
+    find_breakeven,
+)
+from ex05.metrics import InferenceTimer, MetricsResult, RamMonitor
+
 __version__ = "1.0.0"
+__all__ = [
+    # config
+    "ExperimentConfig",
+    "EconomicsConfig",
+    "OllamaConfig",
+    "get_hf_token",
+    # metrics
+    "MetricsResult",
+    "RamMonitor",
+    "InferenceTimer",
+    # economics
+    "compute_api_cost",
+    "compute_onprem_cost",
+    "compute_cloud_gpu_cost",
+    "find_breakeven",
+]
